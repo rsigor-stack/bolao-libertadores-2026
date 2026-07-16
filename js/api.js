db9ff07a40674f7abbbf8283c1758c66feffe373
rsigor-stack <rsigor1040@gmail.com>
2026-07-16 19:37:16 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -6,7 +6,7 @@
  */
 
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbzfOEh6mS_Ja3Cy7TYR4J3U1zPmbbFjMzM9mLa0CVGL8pTRIVMeuMph9um_AsqkxJfodw/exec";
+  "COLE_AQUI_A_URL_DO_WEB_APP_DO_GOOGLE_APPS_SCRIPT";
 
 
 /**
@@ -79,5 +79,117 @@ async function apiRequest(
 
 
   return data;
+
+}
+
+
+/**
+ * ============================================================================
+ * LOGIN
+ * ============================================================================
+ */
+
+async function apiLogin(
+  participante,
+  senha
+) {
+
+  return apiRequest(
+
+    "login",
+
+    {
+
+      method: "POST",
+
+      body: {
+
+        participante,
+
+        senha
+
+      }
+
+    }
+
+  );
+
+}
+
+
+/**
+ * ============================================================================
+ * LISTAR JOGOS
+ * ============================================================================
+ */
+
+async function apiListarJogos() {
+
+  return apiRequest(
+
+    "listarJogos"
+
+  );
+
+}
+
+
+/**
+ * ============================================================================
+ * LISTAR PALPITES
+ * ============================================================================
+ */
+
+async function apiListarPalpites(
+  participante,
+  token
+) {
+
+  return apiRequest(
+
+    "listarPalpites",
+
+    {
+
+      method: "POST",
+
+      body: {
+
+        participante,
+
+        token
+
+      }
+
+    }
+
+  );
+
+}
+
+
+/**
+ * ============================================================================
+ * SALVAR PALPITE
+ * ============================================================================
+ */
+
+async function apiSalvarPalpite(
+  dados
+) {
+
+  return apiRequest(
+
+    "salvarPalpite",
+
+    {
+
+      method: "POST",
+
+      body: dados
+
+    }
+
+  );
 
 }
